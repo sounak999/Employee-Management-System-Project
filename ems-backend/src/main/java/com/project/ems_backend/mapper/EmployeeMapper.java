b1e4f669f9275e42105421aa01f9e914361216bd
Sounak <sounaksaha333@gmail.com>
2025-02-26 12:24:57 +0530

    0.1 => Employee Dto and Mapper class initialization

---
+++ ems-backend/src/main/java/com/project/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.project.ems_backend.mapper;
+
+import com.project.ems_backend.dto.EmployeeDto;
+import com.project.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+}
