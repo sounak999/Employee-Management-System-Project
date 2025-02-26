375d2b46c9b3e9fb78ebef1b362872626ff4ba93
Sounak <sounaksaha333@gmail.com>
2025-02-26 12:21:05 +0530

    0.1 => initializing Employee Reposirty

--- ems-backend/src/main/java/com/project/ems_backend/entity/Employee.java
+++ ems-backend/src/main/java/com/project/ems_backend/entity/Employee.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(name = "first_name")
     private String firstName;
