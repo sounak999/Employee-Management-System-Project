b766be584bc7c4f4a8d1948e66260abdeaabb360
Sounak <sounaksaha333@gmail.com>
2025-03-03 03:08:22 +0530

    0.3 => added sample component to our front-end

---
+++ ems-frontend/src/HelloWorld.jsx
@@ -0,0 +1,10 @@
+function HelloWorld() {
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <p>Welcome to the world of React!</p>
+        </>
+    )
+}
+
+export default HelloWorld;
