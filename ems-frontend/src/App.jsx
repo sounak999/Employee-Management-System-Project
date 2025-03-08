638496551a25026f7bb2beb2d4d5f2c8ef3876c5
Sounak <sounaksaha333@gmail.com>
2025-03-08 21:20:39 +0530

    0.6 => Introducing routing in the front-end

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,15 +2,21 @@ import './App.css'
 import ListEmployeeComponent from './component/ListEmployeeComponent'
 import HeaderComponent from './component/HeaderComponent'
 import FooterComponent from './component/FooterComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<HeaderComponent />
+				<Routes>
+					<Route path="/" element={<ListEmployeeComponent />} />
+					<Route path='/employees' element={<ListEmployeeComponent />} />
+				</Routes>
+				<FooterComponent />
+			</BrowserRouter>
+		</>
+	)
 }
 
 export default App
