9c29258f075a48d6c04d051e6138925c71119925
Sounak <sounaksaha333@gmail.com>
2025-03-08 22:59:10 +0530

    0.7 => Creating Add Employee form

--- ems-frontend/src/component/EmployeeComponent.jsx
+++ ems-frontend/src/component/EmployeeComponent.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EmployeeComponent = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [emailId, setEmail] = useState('')
+
+  function saveEmployee(e) {
+    e.preventDefault();
+    const employee = { firstName, lastName, emailId }
+    console.log(employee)
+  }
+
   return (
-    <div>Employee Component</div>
+    <div className='container col-md-6 offset-md-3'>
+      <div className='row'>
+        <div className='card'>
+          <h2 className='text-center'>Add Employee</h2>
+          <div className='card-body'>
+            <form>
+              <div className='form-group mb-2'>
+                <label htmlFor='firstName' className='form-label'>First Name:</label>
+                <input
+                  type='text'
+                  placeholder='Enter Employee First Name'
+                  id='firstName'
+                  name='firstName'
+                  value={firstName}
+                  className='form-control'
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <label htmlFor='lastName' className='form-label'>Last Name:</label>
+                <input
+                  type='text'
+                  placeholder='Enter Employee Last Name'
+                  id='lastName'
+                  name='lastName'
+                  value={lastName}
+                  className='form-control'
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                <label htmlFor='emailId' className='form-label'>Email Id:</label>
+                <input
+                  type='email'
+                  placeholder='Enter Employee Email Id'
+                  id='emailId'
+                  name='emailId'
+                  value={emailId}
+                  className='form-control'
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
+
 }
 
 export default EmployeeComponent
